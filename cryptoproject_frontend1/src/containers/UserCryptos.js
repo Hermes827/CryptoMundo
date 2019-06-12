4988fe80cc2e8437bd210bf2e40cbee4b5ddcabe
github username <github email address>
2019-06-12 15:03:30 -0700

    implemented total portofolio price feature

--- cryptoproject_frontend1/src/containers/UserCryptos.js
+++ cryptoproject_frontend1/src/containers/UserCryptos.js
@@ -3,11 +3,28 @@ import UserCrypto from '../components/UserCrypto'
 
 class UserCryptos extends React.Component {
 
+getPrices = () => {
+  const values = []
+  this.props.userCryptos.map((crypto)=> {
+    values.push(parseInt(crypto.price))
+  })
+  if(values.length >= 1) {
+  let total = values.reduce((x, y) => x + y)
+  return total
+}
+  let total  = 0
+  return total
+}
+
+
   render(){
     return(
       <div className="userCryptos">
       <h1>My Cryptocurrencies</h1>
+      <h2>Total portfolio value: ${this.getPrices()}</h2>
       <button className="CCbutton" onClick={this.props.returnHome}>Return to homepage</button>
+      {console.log(this.getPrices())}
+
       <div>
       {this.props.userCryptos.map((crypto)=>{
         return <UserCrypto
