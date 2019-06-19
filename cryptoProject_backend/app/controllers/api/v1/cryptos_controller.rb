275d96d5bfa1516049fa87e337a756a69432420b
github username <github email address>
2019-06-18 17:46:59 -0700

    added index graph

--- cryptoProject_backend/app/controllers/api/v1/cryptos_controller.rb
+++ cryptoProject_backend/app/controllers/api/v1/cryptos_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::CryptosController < ApplicationController
     # response = HTTParty.get("https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&api_key={2eb0a0afcdbd0af89e90104132e9424984ac9324e5c2b62272a6afbe9567cb19}")
     # data = JSON.parse(response.to_s)
     # addCryptos(data)
+    # modify amount of cryptos that API fetches by changing number after limit
 
     render json: Crypto.all
     # render json: data
