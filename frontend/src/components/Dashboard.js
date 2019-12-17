aea1801fca641193964172913029f0d5c33ac536
github username <github email address>
2019-12-17 15:23:30 -0800

    fixed misc things

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,13 +3,6 @@ import {Link, withRouter} from 'react-router-dom'
 
 class Dashboard extends Component {
 
-  constructor(){
-    super()
-    this.state = {
-      nothingHasBeenClicked: true
-    }
-  }
-
   userIsLoggedIn(){
     return this.props.current_user.username
   }
