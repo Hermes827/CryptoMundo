ee1b7fcd23aa700839d8b9fd8a57c7eab0dc7d9a
github username <github email address>
2019-12-17 19:41:58 -0800

    login css cleaned up

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,13 +2,8 @@ import React, { Component } from 'react'
 
 export default class Login extends Component {
 
-  
-
   constructor(props){
     super(props)
-    this.state = {
-
-    }
 
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -19,32 +14,27 @@ export default class Login extends Component {
       username: ev.target.elements['username'].value,
       password: ev.target.elements['password'].value
     }
-
     this.props.attemptLogin(user)
   }
 
   render(){
     return (
-      <div className="user-form">
-        <h2 className="title subtitle">Login to Crypto Mundo</h2>
-        <form className="login" onSubmit={this.handleSubmit}>
-          <div>
+      <div className="login-form">
+        <h2 className="login-title">Login to Crypto Mundo</h2>
+        <form onSubmit={this.handleSubmit}>
             <div>
-              <label className="login">Username&nbsp;&nbsp;
+              <label>Username&nbsp;&nbsp;
                 <input className="input" type="text" name="username" placeholder="username"/>
               </label>
             </div>
             <br/>
             <div>
-              <label className="login">Password&nbsp;&nbsp;
+              <label>Password&nbsp;&nbsp;
                 <input className="input" type="password" name="password" placeholder="password"/>
               </label>
             </div>
-          </div>
           <br/>
-
           <button className="login-button" type="submit" value="Login">login</button>
-
         </form>
       </div>
     )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
