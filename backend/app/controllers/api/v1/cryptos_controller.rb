40d5b2b201cc6360dc71423c75f7bb190a6c1f35
github username <github email address>
2019-12-20 11:22:13 -0800

    cleaned up backend code

--- backend/app/controllers/api/v1/cryptos_controller.rb
+++ backend/app/controllers/api/v1/cryptos_controller.rb
@@ -6,16 +6,12 @@ class Api::V1::CryptosController < ApplicationController
   end
 
   def searchByName
-
-    destroy
-
+    Crypto.all.delete_all
     response = HTTParty.get("https://min-api.cryptocompare.com/data/top/mktcapfull?limit=50&tsym=USD&api_key={2eb0a0afcdbd0af89e90104132e9424984ac9324e5c2b62272a6afbe9567cb19}")
     data = JSON.parse(response.to_s)
     addCryptos(data)
     # modify amount of cryptos that API fetches by changing number after limit
-
     render json: Crypto.all
-    # render json: data
   end
 
   def addCryptos(data)
